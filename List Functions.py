4efdd496f6c61f0de52fc0be4615b30c719d30f1
M.Aravindharaj <144996705+Aravindharaj2k3@users.noreply.github.com>
2024-07-11 17:33:35 +0530

    List Functions
    We all the list functions used for this file.

---
+++ List Functions.py
@@ -0,0 +1,46 @@
+'''
+#append
+my_list=[1,3,5,7,8]
+my_list.append(4)
+print(my_list)
+
+#extend
+list1=[1,3,4,5]
+list2=[4,5,6,7]
+list1.extend(list2)
+print(list1)
+
+#insert remove 
+my_list.insert(4,9)
+print(my_list)
+my_list.remove(9)
+print(my_list)
+my_list.count(2)
+print(my_list)
+
+#Reverse
+a=[1,2,3,4,5,6,7,8]
+list.reverse(1,2,3,4,5,6,7,8)
+print(list)
+
+#max
+
+max_value =[54,66,58,22,44,56,58,33,68,45]
+mx = max(max_value)
+print('Print the max value:',mx)
+
+#min
+
+max_value =[54,66,58,22,44,56,58,33,68,45]
+mx = min(max_value)
+print('Print the min value:',mx)
+'''
+#len
+
+str1 ='Aravidharaj'
+count_str = len(str1)
+print('Total count of string',count_str)
+
+
+
+
