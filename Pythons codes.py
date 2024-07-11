412ffdede649277b42071d4a9536f4608edb6d10
M.Aravindharaj <144996705+Aravindharaj2k3@users.noreply.github.com>
2024-07-11 17:18:08 +0530

    Condition wise types of questions
    I am working on string concepts, comparisons, conditional statements, and for loop concepts.

---
+++ Pythons codes.py
@@ -0,0 +1,103 @@
+#concatenate_strings
+'''star=("Aravind")
+star1=("haraj")
+star2=star+star1
+print(star2)'''
+
+#first,mid,last
+'''string=('sdf')
+print(string.len())'''
+
+#count in vowels
+'''string="aravind"
+vow=0
+for i in string:
+    if (i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='e' or i=='I' or i=='O' or i=='U'):
+      vow=vow+1
+print("vowels count i string",vow)
+print(string[::-1])'''
+
+
+#string reverse
+'''def aravind(aravindharaj):
+    return aravindharaj[::-1]
+ara=reverse_string(aravind)
+print("reverse string:",ara)'''
+#white space
+'''a=("  Helloworld")
+print(a.strip())'''
+
+#comparrsion
+'''a=input('enter a value a:')
+b=input('enter a value b:')
+if a==b:
+    print('true')
+else:
+    print('false')''
+
+
+#Conditional satements
+#1,Avearge fuctions
+'''a=int(input("Enter a number:"))
+if a>=90:
+      print("1st grade")
+elif a>=80:
+    print("2nd grade")
+elif a>=70:
+    print("3rd grade")
+else:
+    print("below")'''
+    
+#vote elegiable
+
+'''a=int(input("Enter a number:"))
+if a>=18:
+    print('vote elegiable')
+else:
+    print('not elegiable')'''
+
+'''tax=0.8/0.95
+trip=0.15/0.95
+total=tax+trip
+print('Tax coast:',tax)
+print('trip coast:',trip)
+print('total coast:',total)'''
+
+#forloop
+#natural number
+'''for i in range(1,10,1):
+    print(i)'''
+
+'''num=1634
+oredr=len(str())
+print('this is oredr:',order)
+sum=0
+temp=num
+while temp>0:
+    digit=temp%10
+    sum=sum+digit **
+    temp//=10
+print(sum)
+if num ==sum:
+    print('its a amstrong number',num)
+    else:
+    print('its not a amstrong',num)
+
+
+'''
+
+'''a=[1,2,3,4,6,14]
+large=0
+for i in a:
+    i=>large:
+        large=large+1
+print(i)'''
+
+list1 = [1,2,3,4]
+list2 = ['aravind','aru','raju','kumar']
+for i in list1:
+    for j in list2:
+        print(str(i)+'.'+j)
+
+
+
