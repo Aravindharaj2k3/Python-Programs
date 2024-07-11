bda6544b1dd06d6425f5b893a81d81e0f07857f5
M.Aravindharaj <144996705+Aravindharaj2k3@users.noreply.github.com>
2024-07-11 18:15:50 +0530

    Update Python datatypes.py

--- Python datatypes.py
+++ Python datatypes.py
@@ -1,5 +1,5 @@
-#List Functions  
-'''
+ #List Functions  
+
 #append
 my_list=[1,3,5,7,8]
 my_list.append(4)
@@ -29,8 +29,8 @@ a=[1,2,3,4,5,6,7,8]
 list.reverse(1,2,3,4,5,6,7,8)
 print(list)
 
+#Tuples functions 
 #max
-
 max_value =[54,66,58,22,44,56,58,33,68,45]
 mx = max(max_value)
 print('Print the max value:',mx)
@@ -40,12 +40,49 @@ print('Print the max value:',mx)
 max_value =[54,66,58,22,44,56,58,33,68,45]
 mx = min(max_value)
 print('Print the min value:',mx)
-'''
+
 #len
 
 str1 ='Aravidharaj'
 count_str = len(str1)
 print('Total count of string',count_str)
+
+#Sorting
+data =[51,55,85,44,55,1,2,4,2,7,5,4,]
+data.sort()
+print('sorting the list of numbers',data)
+
+#sum
+data =[51,55,85,44,55,1,2,4,2,7,5,4,]
+sum_value = sum(data)
+print('Sum of value',sum_value)
+
+#List of set functions 
+#union
+
+data1 ={512,84587,5544,7844,144,1144,44411,5545}
+data2 ={154,55,11,22,44554,5544,144,512}
+uni_set = data1.union(data2)
+print('List of union values',uni_set)
+
+#Intersection
+
+data1 ={512,84587,5544,7844,144,1144,44411,5545}
+data2 ={154,55,11,22,44554,5544,144,512}
+uni_set = data1.intersection(data2)
+print('List of common values',uni_set)
+
+#add
+data ={1,2,3,4}
+data.add(6)
+print('Add the values',data)
+
+
+#remove
+data ={1,2,3,4}
+data.remove(3)
+print('Remove the values',data)
+
 
 
 
